e9cc106fa8f4d163753d8c8bacd8c60951a3d29a
katy <katyTsai.git@gmail.com>
2015-12-01 11:57:08 +0800

    change less

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,12 @@ module.exports = {
     publicPath:'/build/'
   },
   resolve:{
-    extensions: ['', '.jsx', '.scss', '.js', '.json']
+    extensions: ['', '.jsx', '.less', '.js', '.json']
   },
   module:{
   loaders:[
     {test:/\.css$/,loader:'style-loader!css-loader'},
-    {test: /\.scss$/,loaders: ['style', 'css', 'sass']},
+    {test: /\.scss$/,loaders: ['style', 'css', 'less']},
     {test:/\.js[x]?$/,exclude:/node_modules/,loader:'babel-loader',
     query: {
           optional: ['runtime']
